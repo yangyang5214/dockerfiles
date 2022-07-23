de4ef76f4992b5a635bb998aa6e7663fa958120b
beer <beer5214@126.com>
2022-07-23 11:00:41 +0800

    update service name

--- golang-crossbuild/armel/build.sh
+++
@@ -1,8 +0,0 @@
-docker run -it --rm \
-  -v $PWD/../demo:/tmp/demo \
-  -w /tmp/demo \
-  -e CGO_ENABLED=1 \
-  -e GOPROXY="https://goproxy.cn,direct" \
-  beer5215/golang-crossbuild-armel \
-  --build-cmd "go build" \
-  -p "linux/arm64"
